6dec1fb340b2a6a0ad2e767e04827b86fe041749
dd231415 <andershofmann@gmail.com>
2023-10-13 11:19:40 +0200

    Update MainMenu.java

--- MainMenu.java
+++ MainMenu.java
@@ -3,18 +3,11 @@
 //import javax.swing.JFrame;
 //import javax.swing.SwingUtilities;
 //import javax.swing.colorchooser.*;
-import javax.swing.*;
+//import javax.swing.*;
 
 
 public class MainMenu {
-    private static void createAndShowGUI() {
-        JFrame frame = new JFrame("Main menu");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+    public static void main(String args[]) {
+        System.out.println("Hello world");
+    }   
 }
-    public static void main(String[] args) {
-    SwingUtilities.invokeLater(() -> createAndShowGUI());
-}
-}
-
